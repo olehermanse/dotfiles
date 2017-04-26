8f22bf9c576b62e950726322397bf5ae9e39abfb
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-04-26 15:13:56 +0200

    Updated cf-net alias to not include sudo

--- cfengine/aliases.sh
+++ cfengine/aliases.sh
@@ -1,7 +1,7 @@
 alias cf-reinstall=${SETUP_BASH_PATH}'/cfengine/reinstall.sh'
 alias cf-aliases='echo "cf-aliases are active"'
 alias cf-kill='bash -c "killall cf-execd ; killall cf-serverd ; killall cf-hub ; killall cf-agent"'
-alias cf-net='sudo /var/cfengine/bin/cf-net'
+alias cf-net='/var/cfengine/bin/cf-net'
 
 alias test-bootstrap=${SETUP_BASH_PATH}'/cfengine/bootstrap.sh'
 alias test-cfnet=${SETUP_BASH_PATH}'/cfengine/cfnet.sh'
