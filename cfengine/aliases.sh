e995fe43b74c47b8a2492937bac708378ff2c35a
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-05-04 16:45:20 +0200

    Small fixes to windows compilation

--- cfengine/aliases.sh
+++ cfengine/aliases.sh
@@ -5,11 +5,13 @@ alias cf-net='/var/cfengine/bin/cf-net'
 
 # alias cf-local-windows-get="rsync -r -l ~/cfengine/windows/core ~/local_cfengine/windows/core"
 
+# INIT local_cfengine folder:
 function cf-local {
     mkdir -p ~/cf_install_win
     rsync -r -l ~/cfengine/ ~/local_cfengine
 }
 
+# Get changes from shared folder using git pull - faster than rsync
 function cf-local-windows-pull {
     cd ~/local_cfengine/windows/core \
     && git pull \
@@ -42,8 +44,9 @@ function cf-local-windows-make {
     && make install
 }
 
+# Copy the binaries from local install directory to shared folder
 function cf-local-windows-put {
-    rsync -r -l ~/cf_install_win ~/cfengine/windows/cf_install_win
+    rsync -r -l ~/cf_install_win ~/cfengine/windows/
 }
 
 function cf-local-windows-all {
