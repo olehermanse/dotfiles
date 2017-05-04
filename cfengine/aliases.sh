41aec22655b227a8eef8ae2c04861d57e10a916e
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-05-04 15:54:41 +0200

    Restructured x-compile functions

--- cfengine/aliases.sh
+++ cfengine/aliases.sh
@@ -10,28 +10,48 @@ function cf-local {
     rsync -r -l ~/cfengine/ ~/cfengine_local
 }
 
+function cf-local-windows-pull {
+    cd ~/cfengine_local/windows/core \
+    && git pull \
+    && cd ~/cfengine_local/windows/enterprise \
+    && git pull
+}
+
 function cf-local-windows-autogen {
-    cd ~/cfengine_local/windows/core NO_CONFIGURE=1 ./autogen.sh
-    cd ~/cfengine_local/windows/enterprise NO_CONFIGURE=1 ./autogen.sh
+    cd ~/cfengine_local/windows/core \
+    && NO_CONFIGURE=1 ./autogen.sh \
+    && cd ~/cfengine_local/windows/enterprise \
+    && NO_CONFIGURE=1 ./autogen.sh
 }
 
 function cf-local-windows-configure {
-    cd ~/cfengine_local/windows/core && ./configure --with-pcre=/var/cfengine --with-openssl=/var/cfengine --with-lmdb=/var/cfengine --with-pthreads=/var/cfengine --host x86_64-w64-mingw32 --prefix=$HOME/cf_install_win
-    cd ~/cfengine_local/windows/enterprise && ./configure --with-pcre=/var/cfengine --with-openssl=/var/cfengine --with-lmdb=/var/cfengine --with-pthreads=/var/cfengine --host x86_64-w64-mingw32 --prefix=$HOME/cf_install_win
-}
-
-function cf-local-windows-pull {
-    cd ~/cfengine_local/windows/core && git pull
-    cd ~/cfengine_local/windows/enterprise && git pull
+    cd ~/cfengine_local/windows/core \
+    && ./configure --with-pcre=/var/cfengine --with-openssl=/var/cfengine \
+       --with-lmdb=/var/cfengine --with-pthreads=/var/cfengine \
+       --host x86_64-w64-mingw32 --prefix=$HOME/cf_install_win \
+    && cd ~/cfengine_local/windows/enterprise \
+    && ./configure --with-pcre=/var/cfengine --with-openssl=/var/cfengine \
+       --with-lmdb=/var/cfengine --with-pthreads=/var/cfengine \
+       --host x86_64-w64-mingw32 --prefix=$HOME/cf_install_win
 }
 
 function cf-local-windows-make {
-    cd ~/cfengine_local/windows/core && make install
-    cd ~/cfengine_local/windows/enterprise && make install
+    cd ~/cfengine_local/windows/core \
+    && make install \
+    && cd ~/cfengine_local/windows/enterprise \
+    && make install
 }
 
 function cf-local-windows-put {
     rsync -r -l ~/cf_install_win ~/cfengine/windows/cf_install_win
+}
+
+function cf-local-windows-all {
+    cf-local-windows-pull \
+    && cf-local-windows-autogen \
+    && cf-local-windows-configure \
+    && cf-local-windows-make \
+    && cf-local-windows-put
 }
 
 alias test-bootstrap=${SETUP_BASH_PATH}'/cfengine/bootstrap.sh'
