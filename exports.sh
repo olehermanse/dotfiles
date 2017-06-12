6527d83b5e4a1d6a94e0d97c6c9a0fd969665d38
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-06-12 20:09:29 +0200

    Enabled bash logging

--- exports.sh
+++ exports.sh
@@ -7,3 +7,4 @@ export GTEST_ROOT=~/code/googletest/googletest
 export GTEST_DIR=~/code/googletest/googletest
 
 export PS1="\\h:\\w $ "
+export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi'
