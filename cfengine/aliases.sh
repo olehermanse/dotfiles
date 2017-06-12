a00de4779f06fcdf6707f05498439046a5adf82a
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-06-12 23:07:37 +0200

    Added alias for printing cfengine processes

--- cfengine/aliases.sh
+++ cfengine/aliases.sh
@@ -1,6 +1,7 @@
 alias cf-reinstall=${SETUP_BASH_PATH}'/cfengine/reinstall.sh'
 alias cf-aliases='echo "cf-aliases are active"'
 alias cf-kill='bash -c "killall cf-execd ; killall cf-serverd ; killall cf-hub ; killall cf-agent"'
+alias cf-procs='ps aux | grep [c]f-'
 
 alias cf-agent="/var/cfengine/bin/cf-agent"
 alias cf-execd="/var/cfengine/bin/cf-execd"
