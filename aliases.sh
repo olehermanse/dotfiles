c71bccfa8a345d7ba967fb283ad16a8aaeab640d
Ole Herman <ole@BlackBox.local>
2017-10-30 12:20:47 -0700

    Added tmux4 command

--- aliases.sh
+++ aliases.sh
@@ -39,6 +39,7 @@ alias bashreinstall='bash <(curl -L -s bit.ly/instsb) && source $HOME/.bash_prof
 alias vmshut="vboxmanage list runningvms | gsed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} VBoxManage controlvm {} acpipowerbutton"
 alias vms="vboxmanage list runningvms"
 
+alias tmux4="tmux new-session \; split-window -h \; split-window -v \; attach"
 
 alias pdflatex='pdflatex -shell-escape'
 
