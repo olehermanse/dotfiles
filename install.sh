7c26adcc0da36239dd3cb4fff0122e5b602b933f
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-11-21 16:09:40 +0100

    Remove old (uninstall)

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mkdir -p ~/tmp
 rm -rf ~/tmp/dotfiles
 rm -rf ~/tmp/dotfiles-master
@@ -12,6 +13,15 @@ cp ~/.dotfiles_olehermanse/.tmux.conf ~/.tmux.conf
 
 touch ~/.bashrc
 touch ~/.bash_profile
-grep -q -F 'source ~/.dotfiles_olehermanse/start.sh' ~/.bashrc || echo 'source ~/.dotfiles_olehermanse/start.sh' >> ~/.bashrc
-grep -q -F 'source ~/.dotfiles_olehermanse/start.sh' ~/.bash_profile || echo 'source ~/.dotfiles_olehermanse/start.sh' >> ~/.bash_profile
+newsource='source ~/.dotfiles_olehermanse/start.sh'
+grep -q -F $newsource ~/.bashrc || echo $newsource >> ~/.bashrc
+grep -q -F $newsource ~/.bash_profile || echo $newsource >> ~/.bash_profile
 rm -rf ~/tmp
+
+# Uninstall old:
+sed -i.bu "/.setup-bash/d" $HOME/.bashrc && rm -f $HOME/.bashrc.bu
+sed -i.bu "/.setup-bash/d" $HOME/.bash_profile && rm -f $HOME/.bashrc.bu
+
+if [ -d ~/.setup-bash ]; then
+   rm -rf ~/.setup-bash
+fi
