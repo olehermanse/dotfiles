42aa615471c78a4636ea64f5f6c684299ae27dea
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2018-11-20 11:08:02 +0100

    Removed cfengine binary aliases

--- cfengine/aliases.sh
+++ cfengine/aliases.sh
@@ -3,16 +3,6 @@ alias cf-aliases='echo "cf-aliases are active"'
 alias cf-kill='bash -c "killall cf-execd ; killall cf-serverd ; killall cf-hub ; killall cf-agent"'
 alias cf-procs='ps aux | grep [c]f-'
 
-alias cf-agent="/var/cfengine/bin/cf-agent"
-alias cf-execd="/var/cfengine/bin/cf-execd"
-alias cf-key="/var/cfengine/bin/cf-key"
-alias cf-monitord="/var/cfengine/bin/cf-monitord"
-alias cf-net="/var/cfengine/bin/cf-net"
-alias cf-promises="/var/cfengine/bin/cf-promises"
-alias cf-runagent="/var/cfengine/bin/cf-runagent"
-alias cf-serverd="/var/cfengine/bin/cf-serverd"
-alias cf-upgrade="/var/cfengine/bin/cf-upgrade"
-
 alias test-bootstrap=${DOTFILES_PATH}'/cfengine/bootstrap.sh'
 alias test-cfnet=${DOTFILES_PATH}'/cfengine/cfnet.sh'
 
