6498b3872a803ce89c13033375dde655fb926dd6
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2019-02-05 14:47:15 +0100

    Counting: End a week if it's been 5 days

--- bin/counting.py
+++ bin/counting.py
@@ -4,15 +4,19 @@ from datetime import datetime, timedelta
 
 TF = "%Y-%m-%d.%H:%M:%S"
 
+
 def str_to_time(string):
     return datetime.strptime(string, TF)
+
 
 def str_from_time(time):
     return datetime.strftime(time, TF)
 
+
 def ints(*args):
     for arg in args:
         yield int(arg)
+
 
 def str_from_delta(time):
     seconds = time.total_seconds()
@@ -20,7 +24,9 @@ def str_from_delta(time):
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 8)
     days, hours, minutes, seconds = ints(days, hours, minutes, seconds)
-    return "{} work days, {} hours, {} minutes, {} seconds".format(days, hours, minutes, seconds)
+    return "{} work days, {} hours, {} minutes, {} seconds".format(
+        days, hours, minutes, seconds)
+
 
 def hours_from_delta(time):
     seconds = time.total_seconds()
@@ -29,24 +35,31 @@ def hours_from_delta(time):
     hours, minutes = ints(hours, minutes)
     return "{}:{} hours".format(hours, minutes)
 
+
 filename = sys.argv[1]
 print("Filename: {}".format(filename))
 data_lines = []
 file_lines = []
+
 
 def data_line(line):
     print(line)
     data_lines.append(line)
     file_lines.append(line)
 
+
 def file_line(line):
     print(line)
     file_lines.append(line)
+
 
 total = timedelta(0)
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 day_index = 0
 week = timedelta(0)
+
+timestamp = None
+
 with open(filename, "r") as f:
     for line in f:
         line = line.strip()
@@ -60,14 +73,23 @@ with open(filename, "r") as f:
             continue
 
         day = words[0]
-        timestamp = words[1][:-1]
+        days_passed = 0
+
+        new_timestamp = words[1][:-1]
+
+        if timestamp:
+            days_passed = str_to_time(new_timestamp) - str_to_time(timestamp)
+            days_passed = days_passed.days
+
         command = words[2]
+        timestamp = new_timestamp
         assert timestamp == str_from_time(str_to_time(timestamp))
 
         if command == "start":
             # Produce end of week summary if necessary:
             new_index = days.index(day)
-            if new_index < day_index:
+            if week > timedelta(0) and (new_index < day_index
+                                        or days_passed >= 5):
                 file_line(str_from_delta(week))
                 file_line("")
                 week = timedelta(0)
