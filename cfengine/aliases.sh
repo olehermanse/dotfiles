9fa4ed8cb34bb46bebf71876e73d26b540e7e796
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2019-08-16 10:11:58 +0200

    cfbuilder was renamed to cf-builder

--- cfengine/aliases.sh
+++ cfengine/aliases.sh
@@ -6,10 +6,10 @@ alias cf-procs='ps aux | grep [c]f-'
 alias test-bootstrap=${DOTFILES_PATH}'/cfengine/bootstrap.sh'
 alias test-cfnet=${DOTFILES_PATH}'/cfengine/cfnet.sh'
 
-alias ci-core="cd /northern.tech/cfengine/starter_pack && python3 cfbuilder.py --rsync ~/cfe --clean --build --test --core --warnings"
-alias ci-oss="cd /northern.tech/cfengine/starter_pack && python3 cfbuilder.py --rsync ~/cfe --clean --build --test --core --masterfiles --warnings"
-alias ci-ent="cd /northern.tech/cfengine/starter_pack && python3 cfbuilder.py --rsync ~/cfe --clean --build --test --core --masterfiles --ent --warnings"
-alias ci-nova="cd /northern.tech/cfengine/starter_pack && python3 cfbuilder.py --rsync ~/cfe --clean --build --test --core --masterfiles --ent --nova --warnings"
+alias ci-core="cd /northern.tech/cfengine/starter_pack && python3 cf-builder.py --rsync ~/cfe --clean --build --test --core --warnings"
+alias ci-oss="cd /northern.tech/cfengine/starter_pack && python3 cf-builder.py --rsync ~/cfe --clean --build --test --core --masterfiles --warnings"
+alias ci-ent="cd /northern.tech/cfengine/starter_pack && python3 cf-builder.py --rsync ~/cfe --clean --build --test --core --masterfiles --ent --warnings"
+alias ci-nova="cd /northern.tech/cfengine/starter_pack && python3 cf-builder.py --rsync ~/cfe --clean --build --test --core --masterfiles --ent --nova --warnings"
 
 # Git:
 alias cf-git-delete-merged='git branch --merged | egrep -v "(^\*|master|3.12.x|3.10.x|3.7.x)$" | xargs git branch -d'
--- cfengine/git_maintenance.sh
+++ cfengine/git_maintenance.sh
@@ -5,7 +5,7 @@ function cf-git-delete-merged () {
 }
 
 cd /northern.tech/cfengine/starter_pack
-python3 cfbuilder.py --core --ent --nova --buildscr --masterfiles --checkout 3.10.x --fetch --rebase upstream/3.10.x --clean --push
+python3 cf-builder.py --core --ent --nova --buildscr --masterfiles --checkout 3.10.x --fetch --rebase upstream/3.10.x --clean --push
 cd /northern.tech/cfengine/core && cf-git-delete-merged
 cd /northern.tech/cfengine/enterprise && cf-git-delete-merged
 cd /northern.tech/cfengine/nova && cf-git-delete-merged
@@ -13,7 +13,7 @@ cd /northern.tech/cfengine/buildscripts && cf-git-delete-merged
 cd /northern.tech/cfengine/masterfiles && cf-git-delete-merged
 
 cd /northern.tech/cfengine/starter_pack
-python3 cfbuilder.py --core --ent --nova --buildscr --masterfiles --checkout 3.12.x --rebase upstream/3.12.x --clean --push
+python3 cf-builder.py --core --ent --nova --buildscr --masterfiles --checkout 3.12.x --rebase upstream/3.12.x --clean --push
 cd /northern.tech/cfengine/core && cf-git-delete-merged
 cd /northern.tech/cfengine/enterprise && cf-git-delete-merged
 cd /northern.tech/cfengine/nova && cf-git-delete-merged
@@ -21,7 +21,7 @@ cd /northern.tech/cfengine/buildscripts && cf-git-delete-merged
 cd /northern.tech/cfengine/masterfiles && cf-git-delete-merged
 
 cd /northern.tech/cfengine/starter_pack
-python3 cfbuilder.py --core --ent --nova --buildscr --masterfiles --checkout master --rebase upstream/master --clean --push
+python3 cf-builder.py --core --ent --nova --buildscr --masterfiles --checkout master --rebase upstream/master --clean --push
 cd /northern.tech/cfengine/core && cf-git-delete-merged
 cd /northern.tech/cfengine/enterprise && cf-git-delete-merged
 cd /northern.tech/cfengine/nova && cf-git-delete-merged
