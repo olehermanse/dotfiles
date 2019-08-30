287680c9d6775d4937f77b6b5892081fcbe33501
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2019-08-30 11:58:56 +0200

    Improved fuzzing instructions

--- cfengine/fuzz.sh
+++ cfengine/fuzz.sh
@@ -1,10 +1,10 @@
 #/usr/bin/env bash
 
-# Run this script as root on an AWS Ubuntu 16 VM, you can curl it from GitHub:
+# Run this script as root on an AWS Ubuntu 18 VM, you can curl it from GitHub:
+# sudo bash
+# apt update
+# apt upgrade
 # curl -L -s https://raw.githubusercontent.com/olehermanse/dotfiles/master/cfengine/fuzz.sh | bash
-
-apt-get update -y
-apt-get upgrade -y
 
 apt-get install -y python
 apt-get install -y python3-pip
@@ -60,6 +60,21 @@ mkdir /home/ubuntu/afl_outputs/
 cp /home/ubuntu/core/examples/main.cf /home/ubuntu/afl_inputs/
 cp /home/ubuntu/core/examples/mergedata.cf /home/ubuntu/afl_inputs/
 
+# Parallell fuzzing, using afl-multicore (afl-utils):
+# afl-multicore -c afl.conf start 8
+
+# Show stats:
+# watch afl-whatsup afl_outputs/
+
+# Collect crashes from multiple fuzzers:
+# afl-collect -r afl_outputs/ ./collection -- /var/cfengine/bin/cf-promises --log-level CRITICAL -c @@
+
+# Minimize corpus (remove "duplicates", reduce sizes of inputs)
+# WARNING: VERY TIME CONSUMING
+# afl-minimize -c new_corpus --cmin --cmin-mem-limit=500 --tmin --tmin-mem-limit=500 -j 8 ./afl_outputs/ -- /var/cfengine/bin/cf-promises --log-level CRITICAL -c @@
+
+# WITHOUT afl-utils:
+
 # Commands for fuzzing, cf-promises is safer (has less side effects):
 # afl-fuzz -i /home/ubuntu/afl_inputs/ -o /home/ubuntu/afl_outputs/ -- /var/cfengine/bin/cf-promises @@
 # afl-fuzz -i /home/ubuntu/afl_inputs/ -o /home/ubuntu/afl_outputs/ -- /var/cfengine/bin/cf-agent -K @@
@@ -69,13 +84,3 @@ cp /home/ubuntu/core/examples/mergedata.cf /home/ubuntu/afl_inputs/
 # afl-fuzz -i /home/ubuntu/afl_inputs/ -o /home/ubuntu/afl_outputs/ -S fuzzer02 -- /var/cfengine/bin/cf-promises @@
 # afl-fuzz -i /home/ubuntu/afl_inputs/ -o /home/ubuntu/afl_outputs/ -S fuzzer03 -- /var/cfengine/bin/cf-promises @@
 # afl-fuzz -i /home/ubuntu/afl_inputs/ -o /home/ubuntu/afl_outputs/ -S fuzzer04 -- /var/cfengine/bin/cf-promises @@
-
-# Parallell fuzzing, using afl-multicore (afl-utils):
-# afl-multicore -c afl.conf start 8
-
-# Collect crashes from multiple fuzzers:
-# afl-collect -r afl_outputs/ ./collection -- /var/cfengine/bin/cf-promises --log-level CRITICAL -c @@
-
-# Minimize corpus (remove "duplicates", reduce sizes of inputs)
-# WARNING: VERY TIME CONSUMING
-# afl-minimize -c new_corpus --cmin --cmin-mem-limit=500 --tmin --tmin-mem-limit=500 -j 8 ./afl_outputs/ -- /var/cfengine/bin/cf-promises --log-level CRITICAL -c @@
