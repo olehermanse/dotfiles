bcc69a096c4c7e601f9166799f28e2c98294d1c5
Ole Herman Schumacher Elgesem <ole@northern.tech>
2021-11-04 19:51:16 +0100

    Eternal bash history
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- exports.sh
+++ exports.sh
@@ -36,3 +36,18 @@ bind "set menu-complete-display-prefix on"
 bind "set mark-symlinked-directories on"
 bind "set show-all-if-unmodified on"
 bind "TAB: menu-complete"
+
+# https://stackoverflow.com/questions/9457233/unlimited-bash-history
+# Eternal bash history.
+# ---------------------
+# Undocumented feature which sets the size to "unlimited".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=
+export HISTSIZE=
+# export HISTTIMEFORMAT="[%F %T] "
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+# PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
