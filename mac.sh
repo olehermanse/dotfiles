4100a211a19876a388ad2fa5e674ce544bf1529e
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2025-05-12 21:03:39 -0300

    Fixed oopsie
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- mac.sh
+++ mac.sh
@@ -7,12 +7,13 @@ export NDK_ROOT=~/code/NDK
 export ANT_ROOT=/usr/local/Cellar/ant
 export NEW_PROJECTS_DIR=~/code/
 
-if test -d "/Users/olehermanse/Library/Python/3.12/bin/"; then
+if test -d "/Users/olehermanse/Library/Python/3.12/bin/"
+then
     export PATH="/Users/olehermanse/Library/Python/3.12/bin/:$PATH"
 fi
 
 function tex {
-    pdflatex --shell-escape $1 && pdflatex --shell-escape $1 && rm -f *.log *.out *.aux
+    pdflatex --shell-escape "$1" && pdflatex --shell-escape "$1" && rm -f ./*.log ./*.out ./*.aux
 }
 
 function cleaner {
@@ -32,29 +33,30 @@ function cd {
     command cd "$(realpath .)" || return
 }
 
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    . $(brew --prefix)/etc/bash_completion
+if [ -f "$(brew --prefix)/etc/bash_completion" ]
+then
+    . "$(brew --prefix)/etc/bash_completion"
 fi
 
 function notes {
     mkdir -p "$HOME/notes/%Y/"
-    mkdir -p $(date "+$HOME/notes/%Y/")
-    mkdir -p $(date "+$HOME/notes/%Y/%m/")
-    touch $(date "+$HOME/notes/%Y/%m/%Y-%m-%d.md")
-    code $HOME/notes $(date "+$HOME/notes/%Y/%m/%Y-%m-%d.md")
+    mkdir -p "$(date "+$HOME/notes/%Y/")"
+    mkdir -p "$(date "+$HOME/notes/%Y/%m/")"
+    touch "$(date "+$HOME/notes/%Y/%m/%Y-%m-%d.md")"
+    code "$HOME/notes" "$(date "+$HOME/notes/%Y/%m/%Y-%m-%d.md")"
 }
 
 function work {
     TIME="$(date "+%Y-%m-%d.%H:%M:%S")"
-    TS="`date +"%a"` $TIME"
-    if [ $1 == "show" ] && [ $# -eq 1 ]
+    TS="$(date +"%a") $TIME"
+    if [ "$1" == "show" ] && [ $# -eq 1 ]
     then
         counting.py ~/.logs/work.log > ~/.logs/work.log.tmp && cp ~/.logs/work.log.tmp ~/.logs/work.log ;
         less ~/.logs/work.log ;
         diff -q ~/.logs/work.log ~/.logs/work.log.tmp && rm ~/.logs/work.log.tmp
         return 0;
     fi
-    if [ ! $# -eq 0 ]
+    if [ $# -eq 0 ]
     then
         echo "Error: Missing work command" && return 1
         return 0;
@@ -67,7 +69,7 @@ function work {
         echo "$TS: $*" >> ~/.logs/timetable.log
         return 0;
     fi
-    if [ $1 == "start" ] || [ $1 == "stop" ] || [ $1 == "skip" ]
+    if [ "$1" == "start" ] || [ "$1" == "stop" ] || [ "$1" == "skip" ]
     then
         echo "$TS: $*" >> ~/.logs/work.log ;
         echo "$TS: $*" >> ~/.logs/work.log.backup ;
