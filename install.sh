c435ef19522312d5a772bd3ca14399a18be75399
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2025-07-23 16:37:50 +0200

    Fixed branch name
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 mkdir -p ~/tmp
 rm -rf ~/tmp/dotfiles
-rm -rf ~/tmp/dotfiles-master
+rm -rf ~/tmp/dotfiles-main
 curl -L https://github.com/olehermanse/dotfiles/archive/master.zip -o ~/tmp/master.zip
 unzip ~/tmp/master.zip -d ~/tmp/
 
-cd ~/tmp/dotfiles-master || exit 'Unable to cd: ~/tmp/dotfiles-master'
+cd ~/tmp/dotfiles-main || exit 'Unable to cd: ~/tmp/dotfiles-main'
 bash ./local_install.sh
 rm -rf ~/tmp
