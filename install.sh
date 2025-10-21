27f5f1f83b3d032f320e86d3ee66ad2e7d371ecd
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2025-10-21 10:11:37 +0200

    Removed unnecessary trailing slash
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ rm -rf ~/tmp/dotfiles
 rm -rf ~/tmp/dotfiles-main
 rm -rf ~/tmp/dotfiles-master
 curl -L https://github.com/olehermanse/dotfiles/archive/main.zip -o ~/tmp/main.zip
-unzip ~/tmp/main.zip -d ~/tmp/
+unzip ~/tmp/main.zip -d ~/tmp
 
 cd ~/tmp/dotfiles-main || exit 'Unable to cd: ~/tmp/dotfiles-main'
 bash ./local_install.sh
