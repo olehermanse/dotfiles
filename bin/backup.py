8cf52da31aba17e79f5dc25259091d328b688300
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2025-12-30 00:48:50 +0100

    Backup improvements
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- bin/backup.py
+++ bin/backup.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python3
 import os
 import sys
+import json
 import getpass
+import hashlib
 import socket
 from datetime import datetime
+
+
+class BackupUserError(Exception):
+    pass
+
+
+class BackupInternalError(Exception):
+    pass
+
+
+DEFAULT_CONFIG = {
+    "profiles": {
+        "default": {
+            "dirs": [
+                ".config",
+                ".gnupg",
+                ".ssh",
+                ".logs",
+            ]
+        },
+        "large": {
+            "dirs": [
+                ".config",
+                ".gnupg",
+                ".ssh",
+                ".logs",
+                "code",
+                ".secrets",
+            ]
+        },
+    },
+    "password_hashes": [],
+}
 
 README = """
 # Simple backups
@@ -21,10 +56,12 @@ openssl enc -d -aes-256-cbc -pbkdf2 -in .config.tgz.enc -out .config.tgz
 ```
 """
 
+
 def log(msg: str):
     print(msg)
     with open("./log.txt", "a") as f:
         f.write(msg + "\n")
+
 
 def backup_maybe(source, name, destination, password):
     assert name
@@ -43,21 +80,22 @@ def backup_maybe(source, name, destination, password):
 
     cleanup(src)
 
-    # cmd = f"cd '{source}' && tar -cz --to-stdout '{name}' | gpg --no-random-seed-file --encrypt --recipient 1AFB7919E3969F50997E6F0318823502992A2BC4 > '{dst}'"
-    cmd = f"cd '{source}' && tar -cz --to-stdout '{name}' | openssl enc -e -aes-256-cbc -pbkdf2 -pass 'pass:{password}' > '{dst}'"
+    # cmd = f"cd '{source}' && tar -cz '{name}' | gpg --no-random-seed-file --encrypt --recipient 1AFB7919E3969F50997E6F0318823502992A2BC4 > '{dst}'"
+    cmd = f"cd '{source}' && tar -cz '{name}' | openssl enc -e -aes-256-cbc -pbkdf2 -pass 'pass:{password}' > '{dst}'"
     assert "'" in cmd and '"' not in cmd
     cmd = f'bash -c "{cmd}"'
     assert "'" in cmd and '"' in cmd
     command(cmd, censor=password)
 
+
 def confirmation():
     print("OK? ", end="")
     answer = input()
     if answer.lower() not in ["", "yes", "y", "ye"]:
-        sys.exit("Not acknowledged")
+        raise BackupUserError("Not acknowledged")
 
 
-def command(cmd: str, censor = None) -> int:
+def command(cmd: str, censor=None) -> int:
     if censor and censor in cmd:
         log(f"Running:\n    {cmd.replace(censor, "<omitted>")}")
     else:
@@ -65,11 +103,13 @@ def command(cmd: str, censor = None) -> int:
     # confirmation()
     return os.system(cmd)
 
+
 def ensure_dir(path):
     assert os.path.isdir(path) or not os.path.exists(path)
     if not os.path.exists(path):
         command(f"mkdir '{path}'")
     assert os.path.isdir(path)
+
 
 def ensure_file(path, content=None):
     if not os.path.exists(path):
@@ -85,6 +125,7 @@ def ensure_file(path, content=None):
     with open(path, "w") as f:
         f.write(content)
 
+
 def cleanup(directory):
     # print(f"Run cleanup in '{directory}' before backup? (y/N) ", end="")
     # answer = input()
@@ -97,6 +138,7 @@ def cleanup(directory):
 
 def bootstrap():
     script_location = os.path.abspath(os.path.expanduser(sys.argv[0]))
+    print(f"Running backup.py from: {script_location}")
     target_location = os.path.abspath("./backup.py")
     if script_location == target_location:
         log("Note: Standalone mode - skipping installation")
@@ -113,17 +155,44 @@ def bootstrap():
     with open(target_location, "r") as f:
         target = f.read()
     if running == target:
+        log(f"Found matching backup.py: {target_location}")
         return
-    log("Note: Updating backup.py to latest (running) version")
+    log("Note: Updating ./backup.py to latest (running) version")
     with open(target_location, "w") as f:
         f.write(running)
 
+
+def get_config(path: str):
+    if not os.path.exists(path + "config.json"):
+        return DEFAULT_CONFIG
+    with open(path + "config.json", "r") as f:
+        return json.loads(f.read())
+
+
+def get_profile(user, machine, config):
+    key = user + "@" + machine
+    if key in config["profiles"]:
+        return config["profiles"][key]
+    return config["profiles"]["default"]
+
+
+def dump_config(path: str, config):
+    desired = json.dumps(config, indent=2) + "\n"
+    ensure_file(path + "config.json", desired)
+
+
+def small_hash(string):
+    return hashlib.sha256(string.encode("utf-8")).hexdigest()[0:4]
+
+
 def main():
     bootstrap()
-    gpgdir = "./gpg/"
     backups = "./backups/"
     root = os.path.abspath(".") + "/"
     assert os.path.isdir(root)
+
+    if not (os.path.exists(root + "config.json")):
+        print("Warning: No config found, will create default")
 
     user = getpass.getuser()
     assert user and type(user) is str
@@ -142,8 +211,10 @@ def main():
 
     assert root.endswith("/")
     assert backups.endswith("/")
-    destination = f"{root}{backups}tmp-backup-{user}-{machine}-{date}-{timestamp}/".replace("/./", "/")
-    final = f"{root}{backups}backup-{user}-{machine}-{date}-{timestamp}/".replace("/./", "/")
+    destination = f"{root}{backups}tmp-backup-{user}-{machine}-{date}-{timestamp}/"
+    destination = destination.replace("/./", "/")
+    final = f"{root}{backups}backup-{user}-{machine}-{date}-{timestamp}/"
+    final = final.replace("/./", "/")
 
     log(f"User: {user}")
     log(f"Home: {homedir}")
@@ -153,26 +224,45 @@ def main():
     log(f"Backup root: {root}")
     log(f"Backup folder: {final}")
 
-    password = getpass.getpass()
-    confirm = getpass.getpass()
-    if confirm != password:
-        sys.exit("Password doesn't match")
+    config = get_config(root)
+    profile = get_profile(user, machine, config)
 
-    ensure_dir(gpgdir)
+    password = getpass.getpass()
+    if not password or len(password) < 14:
+        raise BackupUserError("Password must be at least 14 characters")
+    pw_hash = small_hash(password)
+    if "password_hashes" not in config:
+        config["password_hashes"] = []
+    if pw_hash not in config["password_hashes"]:
+        print(
+            "New password detected, please confirm by typing the password one more time"
+        )
+        confirm = getpass.getpass()
+        if confirm != password:
+            raise BackupUserError("Not sword doesn't match")
+        config["password_hashes"].append(pw_hash)
+
     ensure_dir(backups)
     ensure_dir(destination)
     ensure_file("log.txt")
     ensure_file("README.md", README)
 
-    backup_maybe(homedir, ".gnupg", destination, password)
-    backup_maybe(homedir, ".ssh", destination, password)
-    backup_maybe(homedir, ".config", destination, password)
-    backup_maybe(homedir, ".secrets", destination, password)
-    backup_maybe(homedir, ".logs", destination, password)
-    backup_maybe(homedir, "code", destination, password)
+    for directory in profile["dirs"]:
+        backup_maybe(homedir, directory, destination, password)
 
+    destination_meta = destination + "meta/"
+    ensure_dir(destination_meta)
+    command(f"gpg -a --export > {destination_meta}/mypubkeys.asc")
+    command(f"gpg --export-ownertrust > {destination_meta}/otrust.txt")
+    command(f"cp '{root}backup.py' '{destination_meta}backup.py'")
+    command(f"cp '{root}log.txt' '{destination_meta}log.txt'")
+    if os.path.exists(root + "config.json"):
+        command(f"cp '{root}config.json' '{destination_meta}config_before.json'")
+    dump_config(destination_meta, config)
+    dump_config(root, config)
     command(f"mv '{destination}' '{final}'")
     log("")
+
 
 if __name__ == "__main__":
     main()
