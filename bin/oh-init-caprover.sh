b0af567933a7a640211efae53c9308619e620f3d
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2026-08-29 23:30:48 +0200

    Improving
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- bin/oh-init-caprover.sh
+++ bin/oh-init-caprover.sh
@@ -4,8 +4,8 @@ set -x
 sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc docker-buildx podman-docker containerd runc | cut -f1)
 
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
+sudo apt update -y
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -24,15 +24,16 @@ sudo apt update
 
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo systemctl status docker
 
 sudo systemctl start docker
 
 sudo docker run hello-world
 
-docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
+sudo docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
 
-npm install -g caprover
+sudo apt install -y npm
+sudo npm install -g caprover
 
+sleep 60
 echo "Please run the following command to complete setup:"
 echo "caprover serversetup"
