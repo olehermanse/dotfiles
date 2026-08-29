0588d7837f3f361c2087f46b4cf1279dc041285b
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2026-08-29 22:49:33 +0200

    chown
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- bin/oh-init-cloud.sh
+++ bin/oh-init-cloud.sh
@@ -5,6 +5,7 @@ sudo useradd -m -d /home/olehermanse -s /bin/bash olehermanse
 sudo adduser olehermanse sudo
 mkdir -p /home/olehermanse/.ssh/
 cp ~/.ssh/authorized_keys /home/olehermanse/.ssh/authorized_keys
+chown -R olehermanse:olehermanse /home/olehermanse/
 cp ~/.dotfiles_olehermanse/config/sudo/* /etc/sudoers.d/
 
 cp ~/.dotfiles_olehermanse/config/sshd/sshd_config /etc/ssh/sshd_config
