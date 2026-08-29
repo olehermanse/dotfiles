44284216757d607f7ae92cd58baf5a7ead5e4777
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2026-08-29 22:52:48 +0200

    dotfiles
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- bin/oh-init-cloud.sh
+++ bin/oh-init-cloud.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+
 set -e
 set -x
+
 sudo useradd -m -d /home/olehermanse -s /bin/bash olehermanse
 sudo adduser olehermanse sudo
+
 mkdir -p /home/olehermanse/.ssh/
 cp ~/.ssh/authorized_keys /home/olehermanse/.ssh/authorized_keys
 chown -R olehermanse:olehermanse /home/olehermanse/
+
 cp ~/.dotfiles_olehermanse/config/sudo/* /etc/sudoers.d/
+sudo -H -u olehermanse bash -c 'curl -L -s https://raw.githubusercontent.com/olehermanse/dotfiles/main/install.sh | bash'
 
 cp ~/.dotfiles_olehermanse/config/sshd/sshd_config /etc/ssh/sshd_config
 systemctl restart sshd
