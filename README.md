b92dd1ffa1b3c056d3761a3b5be7e092e4cd4c7b
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2026-08-29 22:21:42 +0200

    Fixes
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### Quick start
 One line install:
 
-```
+```bash
+sudo apt update && sudo apt install unzip
 curl -L -s https://raw.githubusercontent.com/olehermanse/dotfiles/main/install.sh | bash
 ```
--- cloud_init.sh
+++ cloud_init.sh
@@ -1,4 +1,4 @@
-sudo useradd -m -d /home/username -s /bin/bash username
+sudo useradd -m -d /home/olehermanse -s /bin/bash olehermanse
 mkdir -p /home/olehermanse/.ssh/
 cp ~/.ssh/authorized_keys /home/olehermanse/.ssh/authorized_keys
 cp ~/.dotfiles_olehermanse/config/sshd/sshd_config /etc/ssh/sshd_config
