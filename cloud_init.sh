b180199516d9885a5225962407d6109de923276d
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2026-08-29 22:23:28 +0200

    sudo
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- cloud_init.sh
+++ cloud_init.sh
@@ -1,4 +1,5 @@
 sudo useradd -m -d /home/olehermanse -s /bin/bash olehermanse
+sudo adduser olehermanse sudo
 mkdir -p /home/olehermanse/.ssh/
 cp ~/.ssh/authorized_keys /home/olehermanse/.ssh/authorized_keys
 cp ~/.dotfiles_olehermanse/config/sshd/sshd_config /etc/ssh/sshd_config
